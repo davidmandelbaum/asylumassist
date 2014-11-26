5cbd2563cf62c1e217309c007ccdf67ad13935f5
David Mandelbaum <david.m.mandelbaum@gmail.com>
2014-11-25 19:54:01 -0500

    pdftk now loading on heroku

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,14 @@ module App
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
     config.i18n.fallbacks = [:en]
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      if File.exists?(env_file)
+        YAML.load(File.open(env_file)).each do |k, v|
+          ENV[k.to_s] = v
+        end
+      end 
+    end
   end
 end
